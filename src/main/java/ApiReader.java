98ddaa9d7b948ccb3eb61ffc78f9ae1a7a481a33
ziad harb <zharb@rccl.com>
2019-09-11 17:55:17 -0400

    modified files

--- src/main/java/ApiReader.java
+++ src/main/java/ApiReader.java
@@ -2,12 +2,10 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.Charset;
-
 
 public class ApiReader {
 
@@ -28,4 +26,5 @@ public class ApiReader {
             return result;
         }
     }
+    ////////////////////////////////////
 }
